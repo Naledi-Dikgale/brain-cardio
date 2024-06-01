710fa1ebab6be0652929cbaccc4f4aa477159c10
naledi-dikgale <naledi22khosi@gmail.com>
2024-06-01 21:57:01 +0200

    Add components

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Footer = ({ onPrev, onNext }) => {
+  return (
+    <div className="bg-gray-800 p-4 text-white flex justify-between">
+      <button onClick={onPrev} className="bg-blue-500 px-4 py-2 rounded">Prev</button>
+      <button onClick={onNext} className="bg-blue-500 px-4 py-2 rounded">Next</button>
+    </div>
+  );
+};
+
+export default Footer;
