d900372f1b548d1136ce9d85c2de81f3d6ded94b
naledi-dikgale <naledi22khosi@gmail.com>
2024-06-02 21:14:25 +0200

    Add styling

--- src/components/SplashPage.jsx
+++ src/components/SplashPage.jsx
@@ -8,18 +8,24 @@ const SplashPage = ({ onStart }) => {
   };
 
   return (
-    <div className="flex flex-col items-center justify-center h-screen bg-blue-500 text-white">
-      <div className="text-5xl font-bold mb-4">Tech-30</div>
+    <div className="flex flex-col items-center justify-center h-screen bg-gradient-to-r from-indigo-300  via-purple-400 to-violet-600 text-white">
+      <div className="text-4xl font-nosi mb-4">Tech-30</div>
       <div className="relative">
-  <div className="w-40 h-40 bg-cover bg-center rounded-full" style={{ backgroundImage: `url(/logo.png)` }}>
-    <div className="absolute inset-0 bg-black bg-opacity-50 rounded-full"></div>
-  </div>
-  <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-4xl text-white  text-center w-[90%]">Welcome to the Tech-30!</div>
-</div>
+        <div className="p-12 w-40 h-40 bg-cover bg-center rounded-full" style={{ backgroundImage: `url(/logo.png)` }}>
+          <div className="absolute inset-0 bg-indigo-400 bg-opacity-30 rounded-full"></div>
+        </div>
+      </div>
+      {!showRules && (
+        <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-6xl text-violet-50 text-center font-tang font-bold w-[90vw]" style={{ textShadow: '-1px 0 lime, 0 1px lime, 1px 0 lime, 0 -1px lime' }}>
+          Welcome to the Tech-30!
+        </div>
+      )}
       {!showRules ? (
-        <button onClick={handleShowRules} className="bg-white text-blue-500 px-6 py-3 rounded-full text-xl font-bold">
-          Start
-        </button>
+        <div className="mt-6">
+          <button onClick={handleShowRules} className="bg-white text-blue-500 px-6 py-3 rounded-full text-xl font-bold">
+            Start
+          </button>
+        </div>
       ) : (
         <div className="text-center">
           <div className="mb-4">
@@ -29,7 +35,7 @@ const SplashPage = ({ onStart }) => {
             <p>3. If you explanation is good your team mates will guess the term.</p>
             <p>4. One correct term equals one point</p>
             <p>5. The team with the most points wins!</p>
-            <p>6. Click "start" when you are ready to play.</p>
+            <p>6. Click "Next" when you are ready to play.</p>
           </div>
           <button onClick={onStart} className="bg-white text-blue-500 px-6 py-3 rounded-full text-xl font-bold">
             Next
