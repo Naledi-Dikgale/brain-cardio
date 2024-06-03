9269021ad7c4b1199be3f9a55124ed23e66aea63
naledi-dikgale <naledi22khosi@gmail.com>
2024-06-03 11:12:51 +0200

    Fix bugs

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ const Navbar = ({ duration }) => {
   return (
     <div className="bg-blue-500 p-4 text-white">
       <div className="container mx-auto flex justify-between">
-        <div className="text-2xl font-bold">Tech-30</div>
+        <div className="text-2xl  font-bold">Tech-30</div>
         <div className="text-lg">Countdown: {duration} seconds</div>
       </div>
     </div>
