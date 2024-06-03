460af93ce2ea3428f76906b5260e6487b07ce0a1
naledi-dikgale <naledi22khosi@gmail.com>
2024-06-03 11:12:38 +0200

    Fix bugs

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,15 @@
-import React, { useState } from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
-import Timer from './components/Timer';
-import TermsDisplay from './components/TermsDisplay';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import SplashPage from './components/SplashPage';
+import TermsDisplay from './components/TermsDisplay';
 
 const App = () => {
-  const [started, setStarted] = useState(false);
-  const [count, setCount] = useState(5);
-  const duration = 30;
-
-  const handlePrev = () => {
-    if (count > 1) setCount(count - 1);
-  };
-
-  const handleNext = () => {
-    if (count < 10) setCount(count + 1);
-  };
-
-  const startGame = () => {
-    setStarted(true);
-  };
-
   return (
     <Router>
-      <div className="flex flex-col min-h-screen">
-        {!started ? (
-          <SplashPage onStart={startGame} />
-        ) : (
-          <>
-            <Navbar duration={duration} />
-            <main className="flex-grow container mx-auto p-4">
-              <Timer duration={duration} />
-              <TermsDisplay count={count} />
-            </main>
-            <Footer onPrev={handlePrev} onNext={handleNext} />
-          </>
-        )}
-      </div>
+      <Routes>
+        <Route path="/game" element={<TermsDisplay count={5} />} />
+        <Route path="/" element={<SplashPage />} />
+      </Routes>
     </Router>
   );
 };
