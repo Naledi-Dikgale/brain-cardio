88dc493e5974b64e4d1ed2c1aff7a0bfab204763
naledi-dikgale <naledi22khosi@gmail.com>
2024-06-03 18:56:55 +0200

    Add modal

--- src/components/TermsDisplay.jsx
+++ src/components/TermsDisplay.jsx
@@ -1,6 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import { IoArrowBack } from 'react-icons/io5';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useLocation } from 'react-router-dom';
+import Modal from 'react-modal';
+
+Modal.setAppElement('#root');
 
 const terms = [
   'HTML', 'CSS', 'Tailwind', 'React', 'JavaScript', 
@@ -16,7 +19,9 @@ const TermsDisplay = ({ count }) => {
   const [checkedItems, setCheckedItems] = useState([]);
   const [timer, setTimer] = useState(30);
   const [randomTerms, setRandomTerms] = useState(() => getRandomTerms(count));
+  const [modalIsOpen, setModalIsOpen] = useState(false);
   const navigate = useNavigate();
+  const location = useLocation();
 
   useEffect(() => {
     let interval = null;
@@ -32,7 +37,7 @@ const TermsDisplay = ({ count }) => {
 
   useEffect(() => {
     if (timer === 0) {
-      alert(`Time's up! You checked ${checkedItems.length} items.`);
+      setModalIsOpen(true);
     }
   }, [timer]);
 
@@ -48,18 +53,36 @@ const TermsDisplay = ({ count }) => {
   };
 
   return (
-    <div className="text-xl space-y-2" style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
-      <div style={{ position: 'absolute', top: 0, left: 0 }}>
+    <div className="text-xl space-y-2 flex flex-col justify-center items-center h-screen bg-gradient-to-r from-indigo-300  via-purple-400 to-violet-600 ">
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={() => setModalIsOpen(false)}
+        style={{
+          overlay: {
+            backgroundColor: 'rgba(0, 0, 0, 0.5)',
+          },
+          content: {
+            color: 'lightsteelblue',
+          },
+        }}
+      >
+        <h2>Time's up!</h2>
+        <p>You checked {checkedItems.length} items.</p>
+        <button onClick={() => setModalIsOpen(false)}>Close</button>
+      </Modal>
+      <div className="absolute top-2 left-2">
         <IoArrowBack onClick={() => navigate(-1)} />
       </div>
-      <div>Timer: {timer}</div>
+      <div className="bg-lime-300 bg-opacity-50 text-blue-600 rounded px-12 py-4 text-2xl font-bold" style={{ textShadow: '-1px 0 lime, 0 1px lime, 1px 0 lime, 0 -1px lime'}}>
+        Timer: {timer}
+      </div>
       {randomTerms.map((term, index) => (
         <div key={index} style={{ textDecoration: checkedItems.includes(term) ? 'line-through' : 'none' }}>
           <input type="checkbox" onChange={() => handleCheck(term)} />
           {term}
         </div>
       ))}
-      <button onClick={handleNext}>
+      <button onClick={handleNext} className="bg-white text-blue-500 px-6 py-3 rounded-full text-xl font-bold">
         Next
       </button>
     </div>
