71d987b3b7af83d77866278a0af6cedd343d5ad5
naledi-dikgale <naledi22khosi@gmail.com>
2024-06-06 01:54:56 +0200

    Changed app name

--- src/components/SplashPage.jsx
+++ src/components/SplashPage.jsx
@@ -26,7 +26,7 @@ const SplashPage = () => {
           <ImArrowLeft size={30} />
         </button>
       )}
-      <div className="text-4xl font-nosi mb-4">Tech-30</div>
+      <div className="text-4xl font-nosi mb-4">Brain-Cardio</div>
       <div className="relative">
         <div className="p-12 w-40 h-40 bg-cover bg-center rounded-full" style={{ backgroundImage: `url(/logo.png)` }}>
           <div className="absolute inset-0 bg-indigo-400 bg-opacity-30 rounded-full"></div>
@@ -34,7 +34,7 @@ const SplashPage = () => {
       </div>
       {!showRules && (
         <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-6xl text-violet-50 text-center font-tang font-bold w-[90vw]" style={{ textShadow: '-1px 0 lime, 0 1px lime, 1px 0 lime, 0 -1px lime' }}>
-          Welcome to the Tech-30!
+          Welcome to the Brain-Cardio!
         </div>
       )}
       {!showRules ? (
@@ -51,12 +51,13 @@ const SplashPage = () => {
         <div className="text-center">
           <div className="mb-4">
             <p className="text-xl">Game Rules:</p>
-            <p>1. You have 30 seconds to cover 5 tech terms on display.</p>
-            <p>2. Explain the term to your team mates without saying the actual term.</p>
-            <p>3. If you explanation is good your team mates will guess the term.</p>
-            <p>4. One correct term equals one point</p>
-            <p>5. The team with the most points wins!</p>
-            <p>6. Click "Next" when you are ready to play.</p>
+            <p>1. Choose teams and pick category.</p>
+            <p>2. You have 30 seconds to cover 5 terms on display.</p>
+            <p>3. Explain the term to your team mates without saying the actual term.</p>
+            <p>4. If you explanation is good your team mates will guess the term.</p>
+            <p>5. One correct term equals one point</p>
+            <p>6. The team with the most points wins!</p>
+            <p>7. Click "Next" when you are ready to have some fun.</p>
           </div>
           <motion.button onClick={onStart} className="bg-white text-blue-500 px-6 py-3 rounded-full text-xl font-bold"
           whileHover={{ scale: 1.1, boxShadow: "0px 0px 8px rgb(255, 182, 193)" }}
